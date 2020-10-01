d3dc216801c7597bc13985c627be33ba3b54ca13
chilono <chilono@wokegame.com>
2020-10-01 20:41:39 +0800

    添加踏寻往昔之风战斗模块

--- reroro.py
+++ reroro.py
@@ -1787,6 +1787,78 @@ class reroro:
         # 返回成功作战次数
         return succeed_count
 
+    # 踏寻往昔之风 - 显示活动
+    def fightFa(self, level, count):
+        """
+        fa活动特供
+        踏寻往昔之风
+        """
+        # 寻找作战界面内活动图标
+        result = self.waitIsMatchTemplateEx('fa-main.png')
+        # 点击活动图标，进入活动
+        self.clickMouseAdbCenter(result[1], result[2])
+
+        # 点击时光长河
+        result = self.waitIsMatchTemplateEx('fa-time-river.png')
+        self.clickMouseAdbCenter(result[1], result[2])
+
+        # 活动关卡列表
+        fa_list = [
+            'fa-1',
+            'fa-2',
+            'fa-3',
+            'fa-4',
+            'fa-5',
+            'fa-6',
+            'fa-7',
+            'fa-8'
+        ]
+
+        # 寻找当前关卡图
+        result = self.isMatchTemplateEx(level + '.png', threshold=0.98)
+        # 获取关卡list
+        m_level_list = fa_list
+        # 判断是否找到了关卡图
+        while not result[0]:
+            # 没有找到关卡图，寻找其他锚点图
+            for i_level in m_level_list:
+                # 判断锚点图
+                result = self.isMatchTemplateEx(i_level + '.png',
+                                                threshold=0.98)
+                # 是否找到锚点图
+                if result[0]:
+                    # 找到锚点图，进行位置判断
+                    if fa_list.index(
+                            i_level) > fa_list.index(
+                                level):
+                        # 找到的锚点图大于目标图,向右拉，往左移动
+                        self.swipeMouse(800, 450, 1400, 450, 2000)
+                        # 削减关卡列表，减少判断时间
+                        m_level_list = fa_list[:fa_list.index(i_level)]
+                        # 反转列表方便减少时间
+                        m_level_list.reverse()
+                    elif fa_list.index(
+                            i_level) < fa_list.index(
+                                level):
+                        # 如果小于，向左拉，往右移动
+                        self.swipeMouse(800, 450, 200, 450, 2000)
+                        # 削减关卡列表，减少判断时间
+                        m_level_list = fa_list[
+                            fa_list.index(i_level):]
+                    break
+
+            # 滑动结束，再次判断目标图片
+            result = self.isMatchTemplateEx(level + '.png', threshold=0.98)
+
+        # 寻找关卡结束，找到目标关卡图，点击
+        self.clickMouseAdbCenter(result[1], result[2])
+        time.sleep(3)
+
+        # 进入作战
+        succeed_count = self.agencyFight(count)
+
+        # 返回成功作战次数
+        return succeed_count
     def fightMain(self, level, count):
         """主线关卡模块"""
 
