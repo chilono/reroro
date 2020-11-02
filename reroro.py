e1417825f8feec275d375558c7dd8eb283cc1adc
chilono <chilono@wokegame.com>
2020-11-02 11:12:48 +0800

    感恩庆典每日登录添加

--- reroro.py
+++ reroro.py
@@ -1106,6 +1106,22 @@ class reroro:
             #     result = self.isMatchTemplateEx('zq-mrjl.png')
             #     self.clickMouseAdb((1542, 100))
             #     time.sleep(3)
+            # 感恩庆典每日登录奖励
+            if self.isMatchTemplateEx('gnqd-mrdl.png')[0]:
+                # 点击领取
+                self.clickMouseAdb((1404, 250))
+                self.clickMouseAdb((1404, 350))
+                self.clickMouseAdb((1404, 516))
+                self.clickMouseAdb((1404, 679))
+                time.sleep(3)
+                # 确认领取
+                result = self.isMatchTemplateEx('get_item.png')
+                self.clickMouseAdbCenter(result[1], result[2])
+                time.sleep(3)
+                # 关掉窗口
+                result = self.isMatchTemplateEx('gnqd-mrdl.png')
+                self.clickMouseAdb((1542, 100))
+                time.sleep(3)
             # # 再次判断是否是公告
             # if self.isNotice():
             #     self.clickMouseAdb((1547, 71))
