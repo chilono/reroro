03c99165beaed2689bd10e935037d081c15047b3
chilono <chilono@wokegame.com>
2021-02-12 12:12:56 +0800

    龙门幸运墙抽签功能添加

--- reroro.py
+++ reroro.py
@@ -1163,6 +1163,25 @@ class reroro:
                 result = self.isMatchTemplateEx('suimudianxue_mrdl.png')
                 self.clickMouseAdb((1542, 127))
                 time.sleep(3)
+            # 龙门幸运墙抽签
+            if self.isMatchTemplateEx('xingyunqiang_weixuanze.png')[0]:
+                # 点击领取
+                self.clickMouseAdb((364, 347))
+                self.clickMouseAdb((535, 347))
+                self.clickMouseAdb((710, 347))
+                time.sleep(3)
+                # 点击 确定抽取
+                result = self.isMatchTemplateEx(
+                    'xingyunqiang_yixuanze.png.png')
+                self.clickMouseAdbCenter(result[1], result[2])
+                # 确认领取
+                result = self.isMatchTemplateEx('xingyunqiang_get.png')
+                self.clickMouseAdbCenter(result[1], result[2])
+                time.sleep(3)
+                # 关掉窗口
+                result = self.isMatchTemplateEx('xingyunqiang_weixuanze.png')
+                self.clickMouseAdb((1542, 100))
+                time.sleep(3)
             # # 再次判断是否是公告
             # if self.isNotice():
             #     self.clickMouseAdb((1547, 71))
